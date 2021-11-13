9fc1635282825e326a239e5bcdd627e5188fc1cf
Nakhun Song <nak4627@gmail.com>
2021-11-13 22:18:29 +0900

    Add the tripDate's month info to TripDateSelector

--- components/receipt/ReceiptSelector/ReceiptSelector.tsx
+++ components/receipt/ReceiptSelector/ReceiptSelector.tsx
@@ -24,32 +24,34 @@ export default function ReceiptSelector({
   }, [receipts, selectedIndex])
 
   return (
-    <Scroller
-      targetRef={$target}
-      scrollerRef={$scroller}
-    >
-      {receipts.map((receipt, index) => {
-        const selected = selectedIndex === index
-        const { name, prices } = receipt
-        const ref = (el) => $items.current[index] = el
-        return (
-          <Scroller.Item
-            key={`receipt_item_${index}`}
-            ref={ref}
-            selected={selected}
-            gap="64px"
-          >
-            <div className={styles.item_inner_wrapper}>
-              <div className={styles.name}>
-                {name}
+    <div className={styles.wrapper}>
+      <Scroller
+        targetRef={$target}
+        scrollerRef={$scroller}
+      >
+        {receipts.map((receipt, index) => {
+          const selected = selectedIndex === index
+          const { name, prices } = receipt
+          const ref = (el) => $items.current[index] = el
+          return (
+            <Scroller.Item
+              key={`receipt_item_${index}`}
+              ref={ref}
+              selected={selected}
+              gap="64px"
+            >
+              <div className={styles.item_inner_wrapper}>
+                <div className={styles.name}>
+                  {name}
+                </div>
+                <div className={styles.price}>
+                  {prices}
+                </div>
               </div>
-              <div className={styles.price}>
-                {prices}
-              </div>
-            </div>
-          </Scroller.Item>
-        )
-      })}
-    </Scroller>
+            </Scroller.Item>
+          )
+        })}
+      </Scroller>
+    </div>
   )
 }
--- components/trip/TripDateSelector/TripDateSelector.tsx
+++ components/trip/TripDateSelector/TripDateSelector.tsx
@@ -1,9 +1,9 @@
-// import classNames from 'classnames'
-// import monthMapper from 'config/constant/month.json'
-import { usePrevious } from 'hooks/usePrevious';
-import { useEffect, useState } from 'react';
-import { useQueryClient } from 'react-query';
-import { TripDateType } from 'types/trip.type';
+import monthMapper from 'config/constant/month.json'
+import { usePrevious } from 'hooks/usePrevious'
+import { useEffect, useState } from 'react'
+import { useQueryClient } from 'react-query'
+import { TripDateType } from 'types/trip.type'
+import styles from './TripDateSelector.module.scss'
 import Scroller, { useScroller } from '../Scroller'
 
 type TripDateSelectorProps = {
@@ -24,7 +24,17 @@ export default function TripDateSelector({
   const queryClient = useQueryClient()
   const [innerIndex, setInnerIndex] = useState(selectedIndex)
   const preSelectedIndex = usePrevious(selectedIndex)
-  
+
+  if (selectedIndex !== preSelectedIndex && selectedIndex !== innerIndex) {
+    const selectedTripDate = tripDates?.[selectedIndex]
+    const tripDateId = selectedTripDate?.id ?? null
+    queryClient.setQueryData(
+      ['receipts', tripDateId],
+      selectedTripDate?.receipts ?? []
+    )
+    setInnerIndex(selectedIndex)
+  }
+
   if (selectedIndex !== preSelectedIndex && selectedIndex !== innerIndex) {
     const selectedTripDate = tripDates?.[selectedIndex]
     const tripDateId = selectedTripDate?.id ?? null
@@ -40,28 +50,35 @@ export default function TripDateSelector({
   }, [innerIndex])
 
   return (
-    <Scroller
-      targetRef={$target}
-      scrollerRef={$scroller}
-    >
-      {tripDates?.map((tripDate, index) => {
-        const { date } = tripDate
-        const selected = selectedIndex === index
-        const dateInfo = date.split('-')
-        const day = dateInfo?.[2]
-        const ref = (el) => $items.current[index] = el
-        return (
-          <Scroller.Item
-            key={`trip_date_${index}`}
-            ref={ref}
-            selected={selected}
-          >
-            <div style={{ fontSize: '64px' }}>
-              {day}
-            </div>
-          </Scroller.Item>
-        )
-      })}
-    </Scroller>
+    <div className={styles.wrapper}>
+      {tripDates && (
+        <div className={styles.month}>
+          {monthMapper[tripDates[innerIndex].date.split('-')[1]]}
+        </div>
+      )}
+      <Scroller
+        targetRef={$target}
+        scrollerRef={$scroller}
+      >
+        {tripDates?.map((tripDate, index) => {
+          const { date } = tripDate
+          const selected = selectedIndex === index
+          const dateInfo = date.split('-')
+          const day = dateInfo?.[2]
+          const ref = (el) => $items.current[index] = el
+          return (
+            <Scroller.Item
+              key={`trip_date_${index}`}
+              ref={ref}
+              selected={selected}
+            >
+              <div style={{ fontSize: '64px' }}>
+                {day}
+              </div>
+            </Scroller.Item>
+          )
+        })}
+      </Scroller>
+    </div>
   )
 }
